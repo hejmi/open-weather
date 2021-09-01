28e05538388904d50a269d04d07bf0b26f1bcaff
Michael <michael@graphicdesire.se>
2021-09-01 18:01:18 +0200

    temperature graphic

--- src/components/MyWeather.js
+++ src/components/MyWeather.js
@@ -6,11 +6,10 @@ import "./myweather.css";
 export class MyWeather extends Component {
 	render() {
 		const weatherArray = this.props.wdata;
-		const paddingLeft = { marginLeft: "2rem", paddingLeft: weatherArray.main.temp * 3 };
 		return (
 			<section>
 				<div>
-					<h1 className="title">Your Weather</h1>
+					<h1 className="title">iWeather</h1>
 					<p>
 						<strong>{weatherArray.name}</strong>
 						<br />
@@ -25,14 +24,17 @@ export class MyWeather extends Component {
 						<strong>{weatherArray.main.temp}°C</strong> <span className="smaller">(feels like {weatherArray.main.feels_like}°C)</span>
 						<br />
 					</p>
-					<div className="temperature-gradient"> </div>
-					<div className="temperature-arrow" style={paddingLeft}>
-						⇪
+					<div className="temp-outline">
+						<div className="filled-circle"></div>
+						<div className="temperature"></div>
 					</div>
+					<div className="temperature-gradient"> </div>
+					<Animate className="temperature" play duration="1" easeType="ease-Out" start={{ marginLeft: "2rem", paddingLeft: 10 }} end={{ marginLeft: "2rem", paddingLeft: weatherArray.main.temp * 3 }}>
+						⇪
+					</Animate>
 				</div>
 			</section>
 		);
 	}
 }
-
 export default MyWeather;
