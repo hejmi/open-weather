47d29aed5db81a45a0a56402e1412ff0edd58e63
MichMatch <michael@graphicdesire.se>
2021-09-01 10:22:56 +0200

    api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useCallback } from "react";
+import weatherApi from "./weather_api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	getWeather = async () => {
+		const city = "gothenburg";
+		const apiKey = weatherApi;
+		const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApi}`);
+		return await api_call.json();
+	};
+	state = { weatherData: undefined };
+
+	async componentDidMount() {
+		try {
+			const weatherData = await this.getWeather();
+			this.setState({
+				weatherData: weatherData,
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	render() {
+		console.log(this.state.weatherData);
+		return <></>;
+	}
 }
-
-export default App;
