afd55c61ac4d0b34691a6d14e27ec9aff8e2813e
Michael <michael@graphicdesire.se>
2021-09-02 08:51:53 +0200

    wind degree

--- src/components/MyWeather.js
+++ src/components/MyWeather.js
@@ -5,7 +5,39 @@ import "./myweather.css";
 
 export class MyWeather extends Component {
 	render() {
+		let endColor = "#0000FF";
+		let windDirection = "N";
 		const weatherArray = this.props.wdata;
+		let windDegree = weatherArray.wind.deg;
+		switch (true) {
+			case windDegree === 0:
+			case windDegree === 360:
+				windDirection = "N";
+				break;
+			case windDegree <= 89:
+				windDirection = "NE";
+				break;
+			case windDegree === 90:
+				windDirection = "E";
+				break;
+			case windDegree <= 179:
+				windDirection = "SE";
+				break;
+			case windDegree === 180:
+				windDirection = "S";
+				break;
+			case windDegree <= 269:
+				windDirection = "SW";
+				break;
+			case windDegree === 270:
+				windDirection = "W";
+				break;
+			case windDegree <= 359:
+				windDirection = "NW";
+				break;
+		}
+		if (weatherArray.main.temp >= 15) endColor = "#FFCC00";
+		if (weatherArray.main.temp >= 25) endColor = "#FF0000";
 		return (
 			<section>
 				<div>
@@ -23,15 +55,67 @@ export class MyWeather extends Component {
 						<br />
 						<strong>{weatherArray.main.temp}°C</strong> <span className="smaller">(feels like {weatherArray.main.feels_like}°C)</span>
 						<br />
+						<br />
+						<span className="smaller">
+							Todays highest temperature: <strong>{weatherArray.main.temp_max}°C</strong>
+							<br />
+							Todays lowest temperature: <strong>{weatherArray.main.temp_min}°C</strong>
+						</span>
+						<br />
+					</p>
+					<p>
+						Wind:
+						<br />
+						<strong>
+							{weatherArray.wind.speed} {windDirection}
+						</strong>
 					</p>
 					<div className="temp-outline">
-						<div className="filled-circle"></div>
-						<div className="temperature"></div>
+						<Animate
+							play
+							duration="1"
+							easeType="ease-Out"
+							start={{
+								position: "absolute",
+								left: "15px",
+								bottom: "15px",
+								width: "50px",
+								height: "50px",
+								borderRadius: "50px",
+								backgroundColor: "#0000FF",
+							}}
+							end={{
+								position: "absolute",
+								left: "15px",
+								bottom: "15px",
+								width: "50px",
+								height: "50px",
+								borderRadius: "50px",
+								backgroundColor: endColor,
+							}}></Animate>
+						<Animate
+							play
+							duration="1"
+							easeType="ease-Out"
+							start={{
+								position: "absolute",
+								left: "30px",
+								bottom: "50px",
+								height: "30px",
+								width: "20px",
+								backgroundColor: "#0000FF",
+								borderRadius: "50px",
+							}}
+							end={{
+								position: "absolute",
+								left: "30px",
+								bottom: "50px",
+								height: weatherArray.main.temp * 4,
+								width: "20px",
+								backgroundColor: endColor,
+								borderRadius: "50px",
+							}}></Animate>
 					</div>
-					<div className="temperature-gradient"> </div>
-					<Animate className="temperature" play duration="1" easeType="ease-Out" start={{ marginLeft: "2rem", paddingLeft: 10 }} end={{ marginLeft: "2rem", paddingLeft: weatherArray.main.temp * 3 }}>
-						⇪
-					</Animate>
 				</div>
 			</section>
 		);
