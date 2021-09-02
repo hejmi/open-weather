16e33d91f01fc4696c1a03f8927e25c4ca7093a5
MichMatch <michael@graphicdesire.se>
2021-09-02 14:53:15 +0200

    city change and error handling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
+import FormInput from "./components/FormInput";
 import MyWeather from "./components/MyWeather";
 import weatherApi from "./weather_api";
 
 export default class App extends Component {
-	getWeather = async () => {
-		const city = "gothenburg";
+	getWeather = async (city) => {
+		if (!city) city = "gothenburg";
 		const apiKey = weatherApi;
-		const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+
+		const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`).then((response) => {
+			if (response.status === 404) {
+				alert("City is not valid");
+				throw new Error("Bad response from server");
+			}
+			return response;
+		});
+
 		return await api_call.json();
 	};
 	state = { weatherData: undefined };
 
-	async componentDidMount() {
+	async componentDidMount(city) {
 		try {
-			const weatherData = await this.getWeather();
+			const weatherData = await this.getWeather(city);
 			this.setState({
 				weatherData: weatherData,
 			});
@@ -22,10 +31,19 @@ export default class App extends Component {
 		}
 	}
 
+	changeCity = (formData) => {
+		this.componentDidMount(formData);
+	};
+
 	render() {
 		if (this.state.weatherData !== undefined) {
 			console.log(this.state.weatherData);
-			return <MyWeather wdata={this.state.weatherData} />;
+			return (
+				<div>
+					<MyWeather wdata={this.state.weatherData} />
+					<FormInput functionCallFromParent={this.changeCity.bind(this)} />
+				</div>
+			);
 		}
 
 		return "";
